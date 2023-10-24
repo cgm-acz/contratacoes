db8608746de3f327e88819cf94e23a3dda2d7961
cgm-acz <148889760+cgm-acz@users.noreply.github.com>
2023-10-24 14:24:07 -0300

    Update 2_análise_de_licitações.py

--- pages/2_análise_de_licitações.py
+++ pages/2_análise_de_licitações.py
@@ -1,20 +1,8 @@
 import requests
-import json
+import pandas as pd
 
 url = "http://wstransparencia.pma.es.gov.br/api/programas/anos"
 
-# Faz uma solicitação GET para a URL
-response = requests.get(url, timeout=15)
 
-# Verifica se a solicitação foi bem-sucedida (código de status 200)
-if response.status_code == 200:
-    # Analisa o conteúdo JSON da resposta
-    data = json.loads(response.text)
-
-    # Exibe as informações
-    for item in data:
-        print(f"Nome: {item['Nome']}")
-        print(f"Valor: {item['Valor']}")
-        print()
-else:
-    print(f"A solicitação falhou com o código de status {response.status_code}")
+response = requests.get(url)
+st.write(response.json())
