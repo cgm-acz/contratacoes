1468ee89d21e8122f654c81169dd68878aa7cec8
cgm-acz <148889760+cgm-acz@users.noreply.github.com>
2023-10-24 14:22:30 -0300

    Update 2_análise_de_licitações.py

--- pages/2_análise_de_licitações.py
+++ pages/2_análise_de_licitações.py
@@ -1,15 +1,20 @@
 import requests
-import pandas as pd
-import streamlit as st
+import json
 
-lic = st.number_input("Informe o número da licitação:")
+url = "http://wstransparencia.pma.es.gov.br/api/programas/anos"
 
-if lic:  
-  url = "http://wstransparencia.pma.es.gov.br/api/Licitacao/" + lic
-  
-  response = requests.get(url)
-  
-  data = response.json()
-  
-  df = pd.DataFrame(data)
-  st.write(df)
+# Faz uma solicitação GET para a URL
+response = requests.get(url, timeout=15)
+
+# Verifica se a solicitação foi bem-sucedida (código de status 200)
+if response.status_code == 200:
+    # Analisa o conteúdo JSON da resposta
+    data = json.loads(response.text)
+
+    # Exibe as informações
+    for item in data:
+        print(f"Nome: {item['Nome']}")
+        print(f"Valor: {item['Valor']}")
+        print()
+else:
+    print(f"A solicitação falhou com o código de status {response.status_code}")
