96cf4ab5097e92f758b59f38306d9150a075ea69
cgm-acz <148889760+cgm-acz@users.noreply.github.com>
2023-10-24 13:39:36 -0300

    Update 2_análise_de_licitações.py

--- pages/2_análise_de_licitações.py
+++ pages/2_análise_de_licitações.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 lic = st.input_text("Informe o número da licitação:")
 
-url = "http://wstransparencia.pma.es.gov.br/api/Licitacao/" + lic
-
-response = requests.get(url)
-
-data = response.json()
-
-df = pd.DataFrame(data)
-st.write(df)
+if lic:  
+  url = "http://wstransparencia.pma.es.gov.br/api/Licitacao/" + lic
+  
+  response = requests.get(url)
+  
+  data = response.json()
+  
+  df = pd.DataFrame(data)
+  st.write(df)
